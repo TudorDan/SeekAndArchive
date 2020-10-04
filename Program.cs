78d834517daa01b1ef30008ee24a6b45436e155f
TudorDan <tudor.ist@gmail.com>
2020-10-04 20:13:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the directory and file you want to manipulate.
+            string directoryTarget = args[1];
+            string fileToSearch = args[0];
+
+            SearchForFile(directoryTarget, fileToSearch);
+        }
+
+        public static void SearchForFile(string directory, string fileName)
+        {
+            // Take a snapshot of the file system.  
+            DirectoryInfo dir = new DirectoryInfo(directory);
+
+            if (dir.Exists)
+            {
+                // This method assumes that the application has discovery permissions  
+                // for all folders under the specified path.  
+                IEnumerable<FileInfo> fileList = dir.GetFiles("*.*", SearchOption.AllDirectories);
+
+                //Create the query  
+                IEnumerable<FileInfo> fileQuery =
+                    from file in fileList
+                    where file.Name == fileName
+                    select file;
+
+                //Execute the query. This might write out a lot of files!  
+                foreach (FileInfo fi in fileQuery)
+                {
+                    Console.WriteLine(fi.FullName);
+                }
+                if (!fileQuery.Any())
+                {
+                    Console.WriteLine($"No files named '{fileName}' were found in directory '{directory}'.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Directory '{directory}' was not found!");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# Assignment: Searching for a file
+
+## Task
+
+* Create an application which watches a selected group of files, and automatically creates a compressed archive 
+of them when any of them changes.
+
+* Let the application have two command arguments: the first is the file we are looking for, and the second is 
+the directory we must perform the recursive searching.
+
+* As an output, list the path of the files found matching the name.
+***
